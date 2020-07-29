964eba198257b0f664fed526742467dda5e2bfad
Jaris Rener <jaris@liminalvr.com>
2020-07-29 14:51:15 +1000

    Init.

---
+++ Billboard.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+public class Billboard
+    : MonoBehaviour
+{
+    private Transform _camera;
+
+    void Update ()
+    {
+        if(_camera == null)
+        {
+            if(Camera.main == null)
+                return;
+
+            _camera = Camera.main.transform;
+        }
+
+        transform.LookAt(_camera.position, Vector3.up);
+        transform.Rotate(0, 180, 0);
+    }
+}
---
+++ Rotator.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public Vector3 rotation;
+
+    private void Update()
+    {
+        transform.Rotate(rotation);
+    }
+}
